f2cb3616aa4708aacac182d340beb7d7415d1405
Neelancy1504 <neelancynain0205@gmail.com>
2025-02-09 22:09:13 +0530

    added frontend link in backend

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ const PORT = process.env.PORT || process.env.API_PORT;
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
+app.use(cors({ origin: 'https://video-chat-share-jzxy.vercel.app', credentials: true }));
 
 // Add root route
 app.get('/api', (req, res) => {
